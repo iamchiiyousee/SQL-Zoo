cb32c28c7d5f894c7ee1856c5502a5ece99ef5fd
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-07-13 09:43:21 -0400

    Create selectworld.sql

---
+++ selectworld.sql
@@ -0,0 +1,14 @@
+SELECT name, continent, population FROM world
+
+SELECT name FROM world
+WHERE population>200000000
+
+SELECT name, gdp/population FROM world
+
+WHERE population > 200000000
+
+SELECT name, population/1000000 FROM world
+
+WHERE continent = 'South America'
+
+
